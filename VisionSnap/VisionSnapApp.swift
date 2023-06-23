df3754976c26e446ff7daf378ab3a3284979db21
yihong1120 <1104102241@nkust.edu.tw>
2023-06-23 19:34:40 +0800

    Initial Commit

---
+++ VisionSnap/VisionSnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionSnapApp.swift
+//  VisionSnap
+//
+//  Created by WONG YI HUNG on 2023/6/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionSnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
