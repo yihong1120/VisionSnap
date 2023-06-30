7161faee3e0656de2cb04b3e3f61943549da1a95
yihong1120 <qazsx748596@gmail.com>
2023-07-01 02:43:28 +0800

    Initial UI design

--- VisionSnap/ContentView.swift
+++ VisionSnap/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .topLeading) {
+            // Main content view
+            VStack {
+                switch selectedTab {
+                case 0:
+                    Text("Prediction Page")
+                        .font(.title)
+                        .padding()
+                case 1:
+                    Text("Results Page")
+                        .font(.title)
+                        .padding()
+                case 2:
+                    Text("Classes Page")
+                        .font(.title)
+                        .padding()
+                case 3:
+                    Text("Parametres Page")
+                        .font(.title)
+                        .padding()
+                default:
+                    Text("")
+                }
+                Spacer() // push the content to the top
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+
+            // Sidebar for tabs
+            VStack {
+                Spacer()
+                Button(action: {
+                    self.selectedTab = 0
+                }) {
+                    Text("Prediction")
+                        .font(.title2)
+                        .padding()
+                        .foregroundColor(selectedTab == 0 ? .blue : .gray)
+                }
+                
+                Button(action: {
+                    self.selectedTab = 1
+                }) {
+                    Text("Results")
+                        .font(.title2)
+                        .padding()
+                        .foregroundColor(selectedTab == 1 ? .blue : .gray)
+                }
+                
+                Button(action: {
+                    self.selectedTab = 2
+                }) {
+                    Text("Classes")
+                        .font(.title2)
+                        .padding()
+                        .foregroundColor(selectedTab == 2 ? .blue : .gray)
+                }
+                
+                Button(action: {
+                    self.selectedTab = 3
+                }) {
+                    Text("Settings")
+                        .font(.title2)
+                        .padding()
+                        .foregroundColor(selectedTab == 3 ? .blue : .gray)
+                }
+                Spacer(minLength: 50) // adjust this padding to move your tab bar up and down
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.white.ignoresSafeArea())
     }
 }
 
